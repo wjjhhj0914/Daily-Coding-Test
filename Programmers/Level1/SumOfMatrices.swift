a14401cdf4d8d06aa6fac5938e92999933b3afeb
wjjhhj0914 <wjjhhj0914@gmail.com>
2026-02-18 15:24:18 +0900

    [Programmers] Level 1 - 행렬의 덧셈

---
+++ Programmers/Level1/SumOfMatrices.swift
@@ -0,0 +1,11 @@
+func solution(_ arr1: [[Int]], _ arr2: [[Int]]) -> [[Int]] {
+  var result = arr1
+
+  for i in 0..<arr1.count {
+    for j in 0..<arr2[i].count {
+      result[i][j] = arr1[i][j] + arr2[i][j]
+    }
+  }
+
+  return result
+}
